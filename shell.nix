46bc2b5bc3963464c0e3aae3f9dbf4ac83f4a7af
oroguh <hugo.rivera2319@gmail.com>
2015-01-23 10:05:30 -0700

    fixed *nix

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc763
+}:
+let
+  hsEnv = haskellPackages.ghcWithPackages (hsPkgs : (with hsPkgs ; [
+#       safe to remove these 3
+    hlint
+    ghcMod
+    hdevtools
+
+    parsec
+    derive
+    haskeline
+  ]));
+in
+haskellPackages.cabal.mkDerivation (self: {
+  pname = "scheme";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [
+    hsEnv ];
+  extraCmds = ''
+    $(grep export ${hsEnv.outPath}/bin/ghc)
+  '';
+  meta = {
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
